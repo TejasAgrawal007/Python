d4ab288e1d4a8ac88cd95e737f30df1ab84bbc21
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-02-03 17:09:02 +0530

    Update 01_file.py

--- 9. Chapter9/01_file.py
+++ 9. Chapter9/01_file.py
@@ -1,7 +1,23 @@
-# use open function to read the file content
+'''
+
+    Types of File 
+         1) Text Files
+         2) Binary File
+
+
+         Types of mode's
+            A) r - open for reading
+            B) w - open for write
+            c) a - open for appending
+            d) t - open for updating
+
+'''
+
+# use open function to read the file
+
 # f = open('sample.txt', 'r')
-f = open('sample.txt') # By default the mode is r
-# data = f.read()  
-data = f.read(5)  # Read first 5 char form the file
+f = open('sample.txt')
+# data = f.read()
+data = f.read(5) # Read first 5 char form the string.
 print(data)
-f.close();
+f.close()
