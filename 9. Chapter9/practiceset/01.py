555c0269333b7009797bb2e9db0502f54b6d126c
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-02-05 16:27:22 +0530

    Delete 9. Chapter9/practiceset directory

--- 9. Chapter9/practiceset/01.py
+++
@@ -1,7 +0,0 @@
-f = open('poem.txt')
-t = f.read()
-if 'twinkle' in t:
-    print("Twinkle is present")
-else:
-      print("Twinkle is Not present")
-f.close()
--- 9. Chapter9/practiceset/05.py
+++
@@ -1,9 +0,0 @@
-words = ['Donkey', 'befkuf', 'kaadu']
-
-with open("Q4.txt") as f:
-    content = f.read()
-
-for word in words:
-    content = content.replace(word, "$%^&&^$")
-    with open("Q4.txt", "w") as f:
-         f.write(content)
