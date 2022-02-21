cff457bb5345a37f65c713c3bf28b4f1d2bd1091
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-02-21 16:23:33 +0530

    Update 03.py

--- 7. Chapter 7/practiceset/03.py
+++ 7. Chapter 7/practiceset/03.py
@@ -2,11 +2,11 @@ num = int(input("Enter Your Number: "))
 prime = True
 
 for i in range(2, num):
-    if(num%1 == 0):
+    if(num%i == 0):
         prime = False
         break
 
 if prime:
-    prime("This is a Prime Number")
+    print("This is a Prime Number")
 else:
-    prime("This is not a Prime Number")
+    print("This is not a Prime Number")
