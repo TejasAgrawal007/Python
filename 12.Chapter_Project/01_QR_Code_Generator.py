2f15239057dfc8657b41cfe7af090af6c8eca51d
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-03-06 22:44:46 +0530

    Add files via upload

---
+++ 12.Chapter_Project/01_QR_Code_Generator.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img = qr.make("We are The Best Coder 👻")
+img.save("Mindspace.png")
---
+++ 12.Chapter_Project/02_AudioBook.py
@@ -0,0 +1,24 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+
+
+
+"""VOLUME"""
+# volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+# print (volume)                          #printing current volume level
+# engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+# engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+
+
+"""Saving Voice to a file"""
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
