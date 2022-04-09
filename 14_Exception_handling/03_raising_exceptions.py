337b3140775afed9648b1768f08f861137494ffe
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-04-09 15:59:30 +0530

    Update 03_raising_exceptions.py

--- 14_Exception_handling/03_raising_exceptions.py
+++ 14_Exception_handling/03_raising_exceptions.py
@@ -2,7 +2,7 @@ def increment(num):
     try:
         return int(num) + 1
     except:
-        raise ValueError("This is not good - Harry")
+        raise ValueError("This is not good - Tejas")
 
 a = increment('df364')
 print(a)
